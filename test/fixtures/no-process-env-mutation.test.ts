2e5d17c1d6ddbe5f64ab5cc88713a8e9f2120c85
Felix Arntz <felix.arntz@vercel.com>
2026-09-23 11:56:05 +0200

    ignore process.env mutation in test files

---
+++ test/fixtures/no-process-env-mutation.test.ts
@@ -0,0 +1,13 @@
+export {};
+
+declare const process: { env: Record<string, string | undefined> };
+declare const key: string;
+declare const values: Record<string, string | undefined>;
+
+process.env.NODE_ENV = "test";
+process.env[key] = "test";
+process.env.DEBUG ||= "test";
+process.env = values;
+delete process.env.DEBUG;
+delete process.env[key];
+Object.assign(process.env, values);
